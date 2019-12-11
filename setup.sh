0737213cc5f27f10fbfca2a44b5924b6adab48d1
Justintime50 <justinpaulhammond@gmail.com>
2019-12-11 11:27:26 -0700

    Finished setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-shopt -s dotglob # ensure we can replace the hidden .env file
-docker-compose up --build -d
-sleep 5
 cd laravel
 cp .env.example .env
+docker-compose up --build -d
 echo "Enter your EasyPost API Key: "
 read -r APIKEY
+shopt -s dotglob # ensure we can replace the hidden .env file
 sed -i '' -e "s/EASYPOST_API_KEY=/EASYPOST_API_KEY=$APIKEY/g" .env
+docker exec -it easypost-ui php artisan key:generate
