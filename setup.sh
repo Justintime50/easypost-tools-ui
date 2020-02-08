d58bba57886316b2d77cc9be45dca5207ed09d18
Justintime50 <justinpaulhammond@gmail.com>
2020-02-07 22:45:27 -0700

    Force https and fix setup script

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ cd src || exit
 cp .env.example .env
 sed -i '' -e "s/DB_PASSWORD=password/DB_PASSWORD=$DBUSERPASS/g" .env
 docker-compose up -d --build
+sleep 10
 docker exec -it easypost-ui php artisan key:generate
-sleep 15 # wait for the DB to boot up if we haven't already
+sleep 50 # wait for the DB to boot up if we haven't already
 docker exec -it easypost-ui php artisan migrate
-history -c
