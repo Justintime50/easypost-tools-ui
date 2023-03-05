91bb70e20f11f312d20ec5c258051969d69d65c3
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-03-05 02:17:09 -0700

    fix: setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # The following script will setup the project for the first time
 # To run the project in the future, simply run `docker compose up -d`
 
-main() {
+setup() {
     cd src || exit 1
 
     # Install dependencies
@@ -18,12 +18,26 @@ main() {
     # Spin up the project (assumes you already have Traefik setup)
     docker compose up -d --build --quiet-pull
 
-    cd src || exit 1
-
     # Run database migrations once the database container is up and able to accept connections
-    echo "Waiting for database container to boot before migrating and seeding..."
-    sleep 15
-    composer migrate-seed
+    for ((ATTEMPT = 0; ATTEMPT <= 10; ATTEMPT += 1)); do
+        if ! healthcheck; then
+            echo "Database is not ready for connections with attempt #$ATTEMPT, retrying..."
+            sleep 1
+        else
+            cd src || exit 1
+            composer migrate-seed
+            echo "Setup complete!"
+            exit 0
+        fi
+    done
+
+    exit 1
 }
 
-main
+healthcheck() {
+    docker ps | grep -q easypost-tools-ui-easypost-tools-ui-1
+    docker ps | grep -q easypost-tools-ui-easypost-tools-db-1
+    docker exec -t easypost-tools-ui-easypost-tools-db-1 mysql -uroot -ppassword -e "show databases;" &>/dev/null || false
+}
+
+setup
